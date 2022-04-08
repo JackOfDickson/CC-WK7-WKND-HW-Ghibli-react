e963e34cba9202515b30a4aecbac7d77e4493e28
Jack <jackowendickson@gmail.com>
2022-04-08 13:34:12 +0100

    inital set up

---
+++ src/containers/GhibliBox.js
@@ -0,0 +1,15 @@
+import react, {useState, useEffect} from "react"
+
+const GhibliBox = () => {
+
+
+    return (
+        <div>
+            <h1>Ghibli Film</h1>
+
+
+        </div>
+    )
+}
+
+export default GhibliBox
