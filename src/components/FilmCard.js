9dc9b06ab57b26373d31d2acb38bee2a6cde6310
Jack <jackowendickson@gmail.com>
2022-04-08 14:47:37 +0100

    added year and image onto the cards

--- src/components/FilmCard.js
+++ src/components/FilmCard.js
@@ -1,11 +1,16 @@
 import React from "react";
+import './FilmCard.css'
 
 const FilmCard = ({film}) => {
     
 
 
     return (
-        <h3>{film.title}</h3>
+        <div className="film-card">
+            <h3>{film.title}</h3>
+            <p>{film.release_date}</p>
+            <img src={film.image}/>
+        </div>
     )
 }
 
--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -3,6 +3,20 @@ import FilmCard from "./FilmCard";
 
 const FilmList = ({films}) => {
 
+    //sort films alpabetically
+    films.sort(function (a,b) {
+        const titleA = a.title.toUpperCase();
+        const titleB = b.title.toUpperCase();
+        if (titleA < titleB) {
+          return -1;
+        }
+        if (titleA > titleB) {
+          return 1;
+        }
+      
+        return 0;
+    })
+
     const FilmItems = films.map((film, index) => {
         return <FilmCard film={film} key={index}/>
     })
